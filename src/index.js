1b856c060d4db3c22fd2c9295a739383f533314c
ZakhonaNdwalane <zakhona.chiliza@gmail.com>
2025-03-04 01:46:40 +0200

    updetad JS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 1,
     cursor: "",
   });
 }
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apikey = "c4d9be47o0b370b37f28te5a42babf6c";
   let context =
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4-line poem in basic HTML. Each line must be separated by a <br/>. At the end of the poem, add the signature '<strong>SheCodes AI</strong>'. Do not include any additional text, explanations, or formatting outside of the poem and signature. The signature must be wrapped in <strong> tags to make it bold.";
-  let prompt = `user instruction: Generate a poem about ${instructionInput.value}`;
+  let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
 
   //   console.log("Generating a Poem");
